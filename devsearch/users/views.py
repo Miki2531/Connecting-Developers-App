59ebd4b86c5eafc59200145aa7898e48d72c5782
Miki2531 <mikiyasshemelis46@gmail.com>
2023-07-22 10:40:47 +0300

    signal and viewsa

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
@@ -75,7 +76,7 @@ def userProfile(request, pk):
                'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context)
 
-
+@login_required(login_url='login')
 def userProfile(request):
     profile = request.user.profile
     skills = profile.skill_set.all()
@@ -83,8 +84,16 @@ def userProfile(request):
     context = {'profile': profile, 'skills': skills, 'projects': projects}
     return render(request, 'users/account.html', context)
 
-
+@login_required(login_url='login')
 def editAccount(request):
+    profile = request.user.profile
     form = ProfileForm()
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+
+            return redirect('account')
+
     context = {'form': form}
     return render(request, 'users/profile_edit.html')
