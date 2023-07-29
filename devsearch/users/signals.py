d5de6fd5a29f62e1346f6789623eb591dcbecdce
Miki2531 <mikiyasshemelis46@gmail.com>
2023-07-29 19:02:43 +0300

    set signal notification when user login

--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -4,16 +4,30 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
+from django.core.mail import send_mail
+from django.core import settings
 
 def createProfile(sender, instance, created, **kwargs):
     if created:
         user = instance
-        user = Profile.objects.create(
+        profile = Profile.objects.create(
             user=user,
             username=user.username,
             email=user.email,
             name=user.first_name,
         )
+        subject = "Welcome to devsearch"
+        message = "we are glad to you!"
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently= True
+        )
+
+
 
 def updateUser(sender, instance, created, **kwargs): 
     profile = instance
